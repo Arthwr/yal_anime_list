51e76c62e4b15d70d198cbd4b21e51c4cdd57271
arthur <artyr007@gmail.com>
2025-01-27 21:12:59 +0500

    feat(rating): add numer rating visual when hover over rating for better accuracy

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -3,10 +3,28 @@ document.addEventListener("DOMContentLoaded", () => {
   const links = document.querySelectorAll(".left a");
 
   links.forEach((link) => {
-    if (link.getAttribute("href") === currentPath) {
+    if (link.pathname === currentPath) {
       link.classList.add("active");
     } else {
       link.classList.remove("active");
     }
   });
+});
+
+const rating = document.querySelectorAll(".detect-hover");
+
+rating.forEach((node) => {
+  const span = node.querySelector("span");
+
+  if (span) {
+    node.addEventListener("mouseenter", () => {
+      if (span.dataset.score) {
+        span.textContent = span.dataset.score;
+      }
+    });
+
+    node.addEventListener("mouseleave", () => {
+      span.textContent = "";
+    });
+  }
 });
